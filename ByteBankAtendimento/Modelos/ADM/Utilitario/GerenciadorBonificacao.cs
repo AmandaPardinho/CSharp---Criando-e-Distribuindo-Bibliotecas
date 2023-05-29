01f404c0212bda7c500e7cd86e36a8f6cc5a0432
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-29 15:03:38 -0300

    Projeto inicial do curso

---
+++ ByteBankAtendimento/Modelos/ADM/Utilitario/GerenciadorBonificacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBankAtendimento.Modelos.ADM.Funcionarios;
+
+namespace ByteBankAtendimento.Modelos.ADM.Utilitario
+{
+    public class GerenciadorDeBonificacao
+    {
+        private double _totalBonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.getBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+
+    }
+}

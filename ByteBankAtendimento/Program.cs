63b366d0689656e819f3d21b355507bb151c0af8
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-31 17:50:58 -0300

    Criação da classe Estagiario

--- ByteBankAtendimento/Program.cs
+++ ByteBankAtendimento/Program.cs
@@ -1,7 +1,23 @@
 ﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+using ByteBankAtendimento.Modelos.ADM.Funcionarios;
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
 new ByteBank_Atendimento().AtendimentoCliente();
 
+public class Estagiario : Funcionario
+{
+    public Estagiario(double salario, string cpf) : base(salario, cpf)
+    {
+    }
 
+    public override void AumentarSalario()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override double getBonificacao()
+    {
+        throw new NotImplementedException();
+    }
+}
